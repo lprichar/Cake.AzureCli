1b2aa0092f7dd33d6c0258b3852f14d3775595dc
Lee Richardson <github@leerichardson.com>
2020-02-16 21:40:36 -0500

    Move more logic into Core

---
+++ src/Cake.AzCliCore/CommandFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Cake.AzCliCore
+{
+    public class CommandFinder
+    {
+        public List<CliCommand> GetAllCliCommands(CliProgram cliProgram)
+        {
+            var cliCommands = new List<CliCommand>();
+            GetAllCliCommands(cliProgram.RootCommand, cliCommands);
+            return cliCommands;
+        }
+
+        private void GetAllCliCommands(CliGroup cliGroup, List<CliCommand> cliCommands)
+        {
+            foreach (var subGroup in cliGroup.Subgroups)
+            {
+                GetAllCliCommands(subGroup, cliCommands);
+            };
+            cliCommands.AddRange(cliGroup.Commands);
+        }
+    }
+}
